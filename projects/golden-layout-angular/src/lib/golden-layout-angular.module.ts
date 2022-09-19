8ea709072e5e2c676689172775a8d9f5f25d0498
Roy Stewart <roythomasstewart@gmail.com>
2022-09-19 06:33:44 -0400

    Updated missing imports

--- projects/golden-layout-angular/src/lib/golden-layout-angular.module.ts
+++ projects/golden-layout-angular/src/lib/golden-layout-angular.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { GoldenLayoutComponent } from './golden-layout-component/golden-layout.component';
+import { GoldenLayoutComponent } from './components/golden-layout-component/golden-layout.component';
 
 
 
--- projects/golden-layout-angular/src/public-api.ts
+++ projects/golden-layout-angular/src/public-api.ts
@@ -3,7 +3,7 @@
  */
 
 export * from './lib/golden-layout-angular.service';
-export * from './lib/golden-layout-component/golden-layout.component';
+export * from './lib/components/golden-layout-component/golden-layout.component';
 export * from './lib/golden-layout-angular.module';
 
 export * from './lib/types';
